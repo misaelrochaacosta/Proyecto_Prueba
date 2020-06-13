836ccc975864476d6806c9131b81efa073599628
Misael <a2173720478@alumnos.uat.edu.mx>
2020-06-12 21:28:29 -0600

    agregamos lineas de codigo para recuperar los datos de los texbox  checkbox y textview

--- app/src/main/java/misael/android/calcularcheckbox/MainActivity.java
+++ app/src/main/java/misael/android/calcularcheckbox/MainActivity.java
@@ -3,12 +3,28 @@ package misael.android.calcularcheckbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText e1,e2;
+    private TextView tv1;
+    private CheckBox cb1,cb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1 = (EditText)findViewById(R.id.txt_Num1);
+        e2 = (EditText)findViewById(R.id.txt_Num2);
+        cb1= (CheckBox)findViewById(R.id.cb_Sumar);
+        cb2= (CheckBox)findViewById(R.id.cb_Restar);
+        tv1= (TextView)findViewById(R.id.tv_Resultado);
+    }
+
+    public void Calculos(View view){
+        
     }
 }
