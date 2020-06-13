8f0500e99519346b4eb817d3e4a69beb0a33d2e9
Misael <a2173720478@alumnos.uat.edu.mx>
2020-06-13 01:04:03 -0600

    Proyecto Terminado

--- app/src/main/java/misael/android/calcularcheckbox/MainActivity.java
+++ app/src/main/java/misael/android/calcularcheckbox/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +26,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Calculos(View view){
-        
+        String Num1_String = String.valueOf(e1.getText());
+        String Num2_String = String.valueOf(e2.getText());
+        int Num1_Int = Integer.parseInt(Num1_String);
+        int Num2_Int = Integer.parseInt(Num2_String);
+        String resultado_String = " ";
+        int resultado_int= 0 ;
+
+        if ((cb1.isChecked()) && (cb2.isChecked())){
+                 resultado_int = Num1_Int + Num2_Int;
+                 resultado_String = resultado_String + "La Suma es: "+resultado_int + " /";
+                  resultado_int = Num1_Int - Num2_Int;
+                  resultado_String = resultado_String + " La Resta es: " + resultado_int;
+
+
+        }else {
+            if(cb1.isChecked())
+            {
+                resultado_int = Num1_Int + Num2_Int;
+                resultado_String = resultado_String + "La Suma es: "+resultado_int ;
+            }
+
+            if(cb2.isChecked())
+            {
+                resultado_int = Num1_Int + Num2_Int;
+                resultado_String = resultado_String + " La Resta es: " + resultado_int;
+            }
+        }
+
+        if ((cb1.isChecked()== false) && (cb2.isChecked()== false))
+            Toast.makeText(this,"No a Seleccionado Ninguna Opcion ",Toast.LENGTH_LONG).show();
+
+        tv1.setText(resultado_String);
+
+
     }
 }
